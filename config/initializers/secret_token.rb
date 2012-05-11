79549e0128e664752429f361baa2c6369c2d29be
Dave Doolin <david.doolin@gmail.com>
2012-05-11 14:50:22 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Patentserver::Application.config.secret_token = 'c7ee8d601ac65ae5666c3d39f28485a1ebf80fc92af960c7a1073f27788cb25cb23da420f01d56bfd93e8932d5968c1f2b8dedfe54006ac39841742b10445d77'
