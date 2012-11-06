a8ef282dedb6a10b766288efaf7f7839904627bc
Dave Doolin <david.doolin@gmail.com>
2012-11-06 09:45:35 -0800

    id and class helper methods for templates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,38 @@
 module ApplicationHelper
+
+  def template_id
+    {:id => params[:action] }
+  end
+
+  def template_class
+    unless params[:controller].nil?
+      {:class => "#{params[:controller].gsub(/\//,'-')} #{params[:action]}"}
+    else
+      {:class => "FIXME"}
+    end
+  end
+
+  def is_public_home?
+    if params[:controller] == 'pages' && params[:action] == "index" && !current_user
+      true
+    end
+  end
+
+  # Method for handling message types in Bootstrap.
+  def twitterized_type(type)
+    case type
+      when :alert
+        "warning"
+      when :error
+        "error"
+      when :notice
+        "info"
+      when :success
+        "success"
+      when :notice
+        "notice"
+      else
+        type.to_s
+    end
+  end
 end
