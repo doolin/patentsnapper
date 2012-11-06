493b3de182516ba8919c0821f61730ccfb673199
Dave Doolin <david.doolin@gmail.com>
2012-11-06 08:35:19 -0800

    spec support for cucumber, email and devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,9 @@ Patentserver::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
+  # For Devise
+  config.assets.initialize_on_precompile = false
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
