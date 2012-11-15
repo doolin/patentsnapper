061b805a1559a5e2656b8794984119b8869c525b
Dave Doolin <david.doolin@gmail.com>
2012-11-15 12:09:25 -0800

    added claim model spec

--- spec/models/claim_spec.rb
+++ spec/models/claim_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Claim do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  [:patent, :claim].each do |attr|
+    it "responds to #{attr}" do
+      i = Claim.new
+      i.respond_to?(attr).should be_true
+    end
+  end
+
 end
