b449ef0935e0cd207ed92503575ac69597742c16
Dave Doolin <david.doolin@gmail.com>
2012-11-15 12:08:26 -0800

    changed invpat to allow better spec on attributes

--- spec/models/invpat_spec.rb
+++ spec/models/invpat_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Invpat do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  [:firstname, :lastname, :street, :city, :state, :country, :zipcode, :lat, :lon,
+   :invseq, :patent, :gyear, :appyearstr, :appdatestr, :assignee, :asgnum,
+   :patentclass, :coauthor, :invnum, :lower, :upper].each do |attr|
+    it "responds to #{attr}" do
+      i = Invpat.new
+      i.respond_to?(attr).should be_true
+    end
+  end
+
 end
