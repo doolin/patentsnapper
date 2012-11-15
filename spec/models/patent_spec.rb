e34ca2e11264f30b3db140d70a1197c273c6bbf9
Dave Doolin <david.doolin@gmail.com>
2012-11-15 12:10:18 -0800

    added patent model spec

--- spec/models/patent_spec.rb
+++ spec/models/patent_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Patent do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  [:appdate, :appnum, :apptype, :appyear, :claims, :gdate, :gyear, :kind, :patent].each do |attr|
+    it "responds to #{attr}" do
+      i = Patent.new
+      i.respond_to?(attr).should be_true
+    end
+  end
+
 end
