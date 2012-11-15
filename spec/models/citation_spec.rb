7c5fc2a643903f9d9b86fb34ca9f4825f2b8c50d
Dave Doolin <david.doolin@gmail.com>
2012-11-15 12:09:09 -0800

    added citation model spec

--- spec/models/citation_spec.rb
+++ spec/models/citation_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Citation do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  [:category, :cit_country, :cit_date, :cit_kind, :cit_name, :citation, :citseq, :patent].each do |attr|
+    it "responds to #{attr}" do
+      i = Citation.new
+      i.respond_to?(attr).should be_true
+    end
+  end
+
 end
