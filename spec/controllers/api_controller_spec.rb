6d6db827d96d35a285f350b0b3f003eedaee61c9
Dave Doolin <david.doolin@gmail.com>
2012-11-29 15:05:38 -0800

    s/decscribe/describe/g

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 describe ApiController do
   describe "assignees" do
-    
+
   end
-  
-  decscribe "invpats" do
-    
+
+  describe "invpats" do
+
   end
-  
+
   describe "inventors" do
-    
+
   end
-  
+
   describe "patents" do
-    
+
   end
 end
