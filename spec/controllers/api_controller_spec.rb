6933db9a536e40911dc7d35ace80aebcc16f03b3
Adam Barber <adam@adambarber.tv>
2012-11-29 13:13:55 -0800

    Added database.yml back to git

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe ApiController do
-
+  describe "assignees" do
+    
+  end
+  
+  decscribe "invpats" do
+    
+  end
+  
+  describe "inventors" do
+    
+  end
+  
+  describe "patents" do
+    
+  end
 end
