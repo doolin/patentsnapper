943c06001f9b8fb1bbf38eb1a6df63b6ce618f68
Adam Barber <adam@adambarber.tv>
2012-11-29 12:21:50 -0800

    Fixed tests

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -30,18 +30,18 @@ end
 def sign_up
   delete_user
   visit '/users/sign_up'
-  fill_in "Name", :with => @visitor[:name]
-  fill_in "Email", :with => @visitor[:email]
-  fill_in "Password", :with => @visitor[:password]
-  fill_in "Password confirmation", :with => @visitor[:password_confirmation]
+  fill_in "user[name]", :with => @visitor[:name]
+  fill_in "user[email]", :with => @visitor[:email]
+  fill_in "user[password]", :with => @visitor[:password]
+  fill_in "user[password_confirmation]", :with => @visitor[:password_confirmation]
   click_button "Sign up"
   find_user
 end
 
 def sign_in
   visit '/users/sign_in'
-  fill_in "Email", :with => @visitor[:email]
-  fill_in "Password", :with => @visitor[:password]
+  fill_in "user[email]", :with => @visitor[:email]
+  fill_in "user[password]", :with => @visitor[:password]
   click_button "Sign in"
 end
 
