32232a4bd3ec7ebc9211414b1cbbc67127955db8
Dave Doolin <david.doolin@gmail.com>
2012-11-29 13:45:02 -0800

    added database.yml back with pg for test, assignee feature underway

--- features/step_definitions/assignee_steps.rb
+++ features/step_definitions/assignee_steps.rb
@@ -1,9 +1,10 @@
+
 Given /^I have an assignee record$/ do
-  pending # express the regexp above with the code you wish you had
+  @a = FactoryGirl.create(:assignee)
 end
 
 When /^I visit assignees api endpoint for assignee record$/ do
-  pending # express the regexp above with the code you wish you had
+  visit show_assignee_path(id: @a.assignee, format: "json")
 end
 
 Then /^I should the json representation of the assignee$/ do
