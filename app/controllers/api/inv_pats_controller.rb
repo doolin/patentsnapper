d629b0cdc6a13a5533d14686d2fc8398655e4c33
Adam Barber <adam@adambarber.tv>
2012-12-10 16:33:47 -0800

    group by to invpats

--- app/controllers/api/inv_pats_controller.rb
+++ app/controllers/api/inv_pats_controller.rb
@@ -4,7 +4,7 @@ class Api::InvPatsController < ApiController
     query_params = params.except(:id, :format, :controller, :action)
     @invpats = {} if query_params.empty?
     #@invpats = Invpat.where(query_values_to_upcase(query_params)) unless query_params.empty?
-    @invpats = Invpat.where(query_params) unless query_params.empty?
+    @invpats = Invpat.where(query_params).group(&:lower) unless query_params.empty?
   end
   
 end
