f26ddae0ce2afdcf82631af48f2052c47da91b51
Dave Doolin <david.doolin@gmail.com>
2013-02-07 20:27:35 -0800

    rewrite footer links to be more accurate

--- app/controllers/api/patent_descs_controller.rb
+++ app/controllers/api/patent_descs_controller.rb
@@ -2,7 +2,6 @@ class Api::PatentDescsController < ApiController
   def index
     query_params = params.except(:id, :format, :controller, :action, :key)
     @patent_descs = {} if query_params.empty?
-    @patent_descs = PatDesc.where(query_params) unless query_params.empty?   
+    @patent_descs = PatDesc.where(query_params) unless query_params.empty?
   end
-  
 end
