44bf57331c720136ca4df11d062539aebe7e1562
Bill Yeh <billwyeh@yahoo.com>
2013-02-28 23:18:21 -0800

    added failing request spec for invpats

--- spec/requests/inventors_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-describe Api::InventorsController do
-	it "should respond to lowercase queries" do
-		get 'api/inventors', firstname: 'bill'
-		response.body.should_not == [].to_json
-	end
-end
