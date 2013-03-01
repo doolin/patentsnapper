505e4d900b8f3f315ecaa5552f58dc5e7466da0f
Bill Yeh <billwyeh@yahoo.com>
2013-02-28 23:20:03 -0800

    changed file name to api spec, covers all relations

---
+++ spec/requests/api_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+describe ApiController do
+
+  describe "assignees" do
+    it "should respond to lowercase queries" do
+      get 'api/assignees', assignee: 'unisys corporation'
+      response.body.should_not == [].to_json
+    end
+  end
+
+  describe "invpats" do
+    it "should respond to lowercase queries" do
+      get 'api/invpats', firstname: 'bill'
+      response.body.should_not == [].to_json
+    end
+  end
+
+  describe "inventors" do
+    it "should respond to lowercase queries" do
+      get 'api/inventors', firstname: 'bill'
+      response.body.should_not == [].to_json
+    end
+  end
+
+  describe "patents" do
+    # Currently no text in fields for patent database
+  end
+
+end
