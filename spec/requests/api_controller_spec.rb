8711f1a0ed643846069e0808800f9ea41202ff50
Bill Yeh <billwyeh@yahoo.com>
2013-04-01 21:22:03 -0700

    using fixtures now for request specs

--- spec/requests/api_controller_spec.rb
+++ spec/requests/api_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 describe ApiController do
 
-  # TODO: Doesn't work if test database not loaded. Should use fixtures instead.
+  assignee = FactoryGirl.create(:assignee, assignee: 'UNISYS CORPORATION')
   describe "assignees" do
     it "should respond to lowercase queries" do
       get 'api/assignees', assignee: 'unisys corporation'
@@ -9,6 +9,7 @@ describe ApiController do
     end
   end
 
+  invpat = FactoryGirl.create(:invpat, lastname: 'DURAND')
   describe "invpats" do
     it "should respond to lowercase queries" do
       get 'api/invpats', lastname: 'durand'
@@ -16,6 +17,7 @@ describe ApiController do
     end
   end
 
+  inventor = FactoryGirl.create(:inventor, lastname: 'LIN')
   describe "inventors" do
     it "should respond to lowercase queries" do
       get 'api/inventors', lastname: 'lin'
