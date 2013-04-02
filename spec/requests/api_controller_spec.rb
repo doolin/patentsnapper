558a1de2ecc77fb2dad988209a1b608bea84bc09
Bill Yeh <billwyeh@yahoo.com>
2013-04-01 21:24:02 -0700

    removing git conflict markers

--- spec/requests/api_controller_spec.rb
+++ spec/requests/api_controller_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 describe ApiController do
 
-<<<<<<< HEAD
   assignee = FactoryGirl.create(:assignee, assignee: 'UNISYS CORPORATION')
-=======
-  # TODO: Doesn't work if test database not loaded. Should use fixtures instead.
->>>>>>> ff09f0e524297d3e4bc750cedd259dbcf0850fd3
   describe "assignees" do
     it "should respond to lowercase queries" do
       get 'api/assignees', assignee: 'unisys corporation'
@@ -13,10 +9,7 @@ describe ApiController do
     end
   end
 
-<<<<<<< HEAD
   invpat = FactoryGirl.create(:invpat, lastname: 'DURAND')
-=======
->>>>>>> ff09f0e524297d3e4bc750cedd259dbcf0850fd3
   describe "invpats" do
     it "should respond to lowercase queries" do
       get 'api/invpats', lastname: 'durand'
@@ -24,10 +17,7 @@ describe ApiController do
     end
   end
 
-<<<<<<< HEAD
   inventor = FactoryGirl.create(:inventor, lastname: 'LIN')
-=======
->>>>>>> ff09f0e524297d3e4bc750cedd259dbcf0850fd3
   describe "inventors" do
     it "should respond to lowercase queries" do
       get 'api/inventors', lastname: 'lin'
