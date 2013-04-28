2804b7a785ba8054aed66e8fb27f1d515ba1aef4
Dave Doolin <david.doolin@gmail.com>
2013-04-28 11:24:15 -0700

    fixed api request spec

--- spec/requests/api_controller_spec.rb
+++ spec/requests/api_controller_spec.rb
@@ -1,26 +1,26 @@
 require 'spec_helper'
 describe ApiController do
 
-  # TODO: Doesn't work if test database not loaded. Should use fixtures instead.
   describe "assignees" do
     it "should respond to lowercase queries" do
+      assignee = FactoryGirl.create(:assignee, assignee: 'UNISYS CORPORATION')
       get 'api/assignees', assignee: 'unisys corporation'
       response.body.should_not == [].to_json
     end
   end
 
-  invpat = FactoryGirl.create(:invpat, lastname: 'DURAND')
   describe "invpats" do
     it "should respond to lowercase queries" do
-      get 'api/invpats', firstname: 'bill'
+      invpat = FactoryGirl.create(:invpat, lastname: 'DURAND')
+      get 'api/invpats', lastname: 'durand'
       response.body.should_not == [].to_json
     end
   end
 
-  inventor = FactoryGirl.create(:inventor, lastname: 'LIN')
   describe "inventors" do
     it "should respond to lowercase queries" do
-      get 'api/inventors', firstname: 'bill'
+      inventor = FactoryGirl.create(:inventor, lastname: 'LIN')
+      get 'api/inventors', lastname: 'lin'
       response.body.should_not == [].to_json
     end
   end
