4957a0091fa958ef6c124f6072e828dfd225e826
Dave Doolin <david.doolin@gmail.com>
2013-04-28 16:40:27 -0700

    added pagination to invpats

--- app/controllers/api/inv_pats_controller.rb
+++ app/controllers/api/inv_pats_controller.rb
@@ -1,10 +1,7 @@
 class Api::InvPatsController < ApiController
-  
   def index
-    query_params = params.except(:id, :format, :controller, :action, :key)
+    query_params = params.except(:id, :format, :controller, :action, :key, :page)
     @invpats = {} if query_params.empty?
-    #@invpats = Invpat.where(query_values_to_upcase(query_params)) unless query_params.empty?
-    @invpats = Invpat.where(query_params).order("lower DESC") unless query_params.empty?
+    @invpats = Invpat.where(query_params).order("lower DESC").page(params[:page]).per_page(10) unless query_params.empty?
   end
-  
 end
